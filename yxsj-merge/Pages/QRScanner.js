d727b76439f55c9971345ea0ef1460c3243c6dc3
HiokHian <56589476+HiokHian@users.noreply.github.com>
2020-10-27 14:21:59 +0800

    Update QRScanner.js

--- yxsj-merge/Pages/QRScanner.js
+++ yxsj-merge/Pages/QRScanner.js
@@ -2,53 +2,70 @@ import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { useNavigation } from "@react-navigation/native";
+import { render } from "react-dom";
 
-export default function QRScanner() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
+class QRScanner extends React.Component{
+  // const [hasPermission, setHasPermission] = useState(null);
+  // const [scanned, setScanned] = useState(false);
 
-  useEffect(() => {
+  state = {
+    hasPermission: true,
+    scanned: false,
+  }
+
+  componentDidMount() {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === "granted");
-    })();
-  }, []);
+      var permission = (status ==="granted")
+      this.setState ({
+        hasPermission: permission
+      })
+    })
+  }
+  setScanned = (scanned) => {
+    this.setState ({
+      scanned: scanned
+    })
+  };
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
+
+  handleBarCodeScanned = ({ type, data }) => {
+    this.setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
 
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
   //After scanning, lead to another page
-  if (scanned === true) {
-    navigation.navigate("Deposit");
+  render(){
+    if (this.state.scanned === true) {
+      this.props.navigation.navigate("Deposit");
+    }
+  
+    if (this.state.hasPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+  
+    if (this.state.hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "flex-end",
+        }}
+      >
+        <BarCodeScanner
+          onBarCodeScanned={this.state.scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+  
+        {this.state.scanned && (
+          <Button title={"Tap to Scan Again"} onPress={() => this.setScanned(false)} />
+        )}
+      </View>
+    );
   }
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "flex-end",
-      }}
-    >
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-
-      {scanned && (
-        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
-      )}
-    </View>
-  );
 }
+export default QRScanner
